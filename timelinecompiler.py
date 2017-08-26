a3850cb549a24b96cd4c2e9c646ecb10941d2180
madman-bob <madman.bob@hotmail.co.uk>
2017-08-26 15:37:57 +0100

    Update Python files to follow PEP8

--- timelinecompiler.py
+++ timelinecompiler.py
@@ -4,176 +4,180 @@ import re
 from timelineobjects import *
 
 patterns = {
-	"Pages": re.compile("^\d+(-\d+(-2)?)?$"),
-	"==>": re.compile("^=+>$"),
-	"<==": re.compile("^<=+$"),
-	"GOTO": re.compile("^~\s*[\w ()'^]+$", re.IGNORECASE),
-	"Name": re.compile("^Name:\s*[\w ()'^]+$", re.IGNORECASE),
-	"Colour": re.compile("^Colour:\s*#[0-9A-F]{6}$", re.IGNORECASE),
-	"Image": re.compile("^Image:\s*\w+\.\w+$", re.IGNORECASE),
-	"Group": re.compile("^Group:\s*[\w ()']+$", re.IGNORECASE),
-	"Caption": re.compile("^Caption:\s*[\w ]+$", re.IGNORECASE)
+    "Pages": re.compile("^\d+(-\d+(-2)?)?$"),
+    "==>": re.compile("^=+>$"),
+    "<==": re.compile("^<=+$"),
+    "GOTO": re.compile("^~\s*[\w ()'^]+$", re.IGNORECASE),
+    "Name": re.compile("^Name:\s*[\w ()'^]+$", re.IGNORECASE),
+    "Colour": re.compile("^Colour:\s*#[0-9A-F]{6}$", re.IGNORECASE),
+    "Image": re.compile("^Image:\s*\w+\.\w+$", re.IGNORECASE),
+    "Group": re.compile("^Group:\s*[\w ()']+$", re.IGNORECASE),
+    "Caption": re.compile("^Caption:\s*[\w ]+$", re.IGNORECASE)
 }
 
-def parsePersonFile(fileLocation):
-	indentLevel = 0
-	with open(fileLocation, "r") as personFile:
-		for line in personFile:
-			potentialCommand = line.strip()
-			patternMatch = next((pattern for pattern in patterns if patterns[pattern].match(potentialCommand)), None)
-			if patternMatch:
-				# Good enough in most cases
-				# May want to improve later
-				nextIndentLevel = len(line) - len(line.lstrip())
-				if nextIndentLevel > indentLevel:
-					yield ("BOT",)
-				elif nextIndentLevel < indentLevel:
-					yield ("EOT",)
-				indentLevel = nextIndentLevel
-			
-			if patternMatch == "Pages":
-				command = [patternMatch] + [int(s) for s in potentialCommand.split("-")]
-				if len(command) == 2:
-					command.append(command[1])
-				command[2] += 1
-				yield tuple(command)
-			elif patternMatch == "GOTO":
-				yield (patternMatch, potentialCommand[1:].strip())
-			elif patternMatch in {"Name", "Colour", "Image", "Group", "Caption"}:
-				yield (patternMatch, potentialCommand.split(":")[1].strip())
-			elif patternMatch != None:
-				yield (patternMatch,)
-	yield ("EOT",)
+
+def parse_person_file(file_location):
+    indent_level = 0
+    with open(file_location, "r") as person_file:
+        for line in person_file:
+            potential_command = line.strip()
+            pattern_match = next((pattern for pattern in patterns if patterns[pattern].match(potential_command)), None)
+            if pattern_match:
+                # Good enough in most cases
+                # May want to improve later
+                next_indent_level = len(line) - len(line.lstrip())
+                if next_indent_level > indent_level:
+                    yield ("BOT",)
+                elif next_indent_level < indent_level:
+                    yield ("EOT",)
+                indent_level = next_indent_level
+
+            if pattern_match == "Pages":
+                command = [pattern_match] + [int(s) for s in potential_command.split("-")]
+                if len(command) == 2:
+                    command.append(command[1])
+                command[2] += 1
+                yield tuple(command)
+            elif pattern_match == "GOTO":
+                yield (pattern_match, potential_command[1:].strip())
+            elif pattern_match in {"Name", "Colour", "Image", "Group", "Caption"}:
+                yield (pattern_match, potential_command.split(":")[1].strip())
+            elif pattern_match is not None:
+                yield (pattern_match,)
+    yield ("EOT",)
+
 
 colours = []
 images = []
 groups = []
 
-def parsePersonTokens(commandIterator, previousPages = None, currentPerson = None, currentColour = None, currentImage = None, currentGroup = None, nextCaption = None):
-	# Page to pass into next splinter timeline
-	splinterPages = []
-	# Page returned from splinter timeline
-	returnPage = None
-	
-	if previousPages == None:
-		previousPages = []
-	
-	for command in commandIterator:
-		if command[0] == "Pages":
-			for pageNumber in range(*command[1:]):
-				nextLink = link(pageNumber, currentPerson, currentColour, currentImage, currentGroup)
-				for page in previousPages:
-					page.linkTo(nextLink, nextCaption)
-				previousPages = [nextLink]
-				nextCaption = None
-		elif command[0] == "==>":
-			splinterPages = previousPages
-		elif command[0] == "<==":
-			previousPages.extend(returnPages)
-		elif command[0] == "GOTO":
-			for page in previousPages:
-				page.linkTo(command[1])
-			previousPages = [link(command[1])]
-			nextCaption = None
-		elif command[0] == "EOT":
-			currentPerson.lastPages = previousPages
-			return previousPages
-		elif command[0] == "BOT":
-			returnPages = parsePersonTokens(commandIterator, splinterPages, currentPerson, currentColour, currentImage, currentGroup)
-			splinterPages = []
-		elif command[0] == "Name":
-			currentPerson = getPerson(command[1])
-		elif command[0] == "Colour":
-			if not command[1] in colours:
-				colours.append(command[1])
-			currentColour = colours.index(command[1])
-		elif command[0] == "Image":
-			if not command[1] in images:
-				images.append(command[1])
-			currentImage = images.index(command[1])
-		elif command[0] == "Group":
-			if not command[1] in groups:
-				groups.append(command[1])
-			currentGroup = groups.index(command[1])
-		elif command[0] == "Caption":
-			nextCaption = command[1]
+
+def parse_person_tokens(command_iterator, previous_pages=None, current_person=None, current_colour=None, current_image=None, current_group=None, next_caption=None):
+    # Page to pass into next splinter timeline
+    splinter_pages = []
+    # Page returned from splinter timeline
+    return_page = None
+
+    if previous_pages is None:
+        previous_pages = []
+
+    for command in command_iterator:
+        if command[0] == "Pages":
+            for page_number in range(*command[1:]):
+                next_link = Link(page_number, current_person, current_colour, current_image, current_group)
+                for page in previous_pages:
+                    page.link_to(next_link, next_caption)
+                previous_pages = [next_link]
+                next_caption = None
+        elif command[0] == "==>":
+            splinter_pages = previous_pages
+        elif command[0] == "<==":
+            previous_pages.extend(return_pages)
+        elif command[0] == "GOTO":
+            for page in previous_pages:
+                page.link_to(command[1])
+            previous_pages = [Link(command[1])]
+            next_caption = None
+        elif command[0] == "EOT":
+            current_person.last_pages = previous_pages
+            return previous_pages
+        elif command[0] == "BOT":
+            return_pages = parse_person_tokens(command_iterator, splinter_pages, current_person, current_colour, current_image, current_group)
+            splinter_pages = []
+        elif command[0] == "Name":
+            current_person = get_person(command[1])
+        elif command[0] == "Colour":
+            if not command[1] in colours:
+                colours.append(command[1])
+            current_colour = colours.index(command[1])
+        elif command[0] == "Image":
+            if not command[1] in images:
+                images.append(command[1])
+            current_image = images.index(command[1])
+        elif command[0] == "Group":
+            if not command[1] in groups:
+                groups.append(command[1])
+            current_group = groups.index(command[1])
+        elif command[0] == "Caption":
+            next_caption = command[1]
+
 
 if __name__ == "__main__":
-	# Get order of expected people from designated file
-	# Read timelines in order
-	# Check expected images present
-	
-	currentlocation = path.dirname(__file__)
-	
-	peopleWithFiles = set(listdir(path.join(currentlocation, "Readable Timelines")))
-	expectedPeople = set()
-	
-	# Get information from files
-	with open(path.join(currentlocation, "timelineexpectedpeople.txt"), "r") as peopleFile:
-		for line in peopleFile:
-			if line.strip() != "":
-				name = line.strip() + ".txt"
-				expectedPeople.add(name)
-				if name in peopleWithFiles:
-					parsePersonTokens(parsePersonFile(path.join(currentlocation, "Readable Timelines", name)))
-	
-	# Pass through, replacing links from relative locations with absolute locations
-	# (Note: still have links to relative locations)
-	for personName in people:
-		if personName in nextPageLinks:
-			for page in nextPageLinks[personName]:
-				for nextLink in page.nextLinks:
-					for lastPage in people[personName].lastPages:
-						lastPage.linkTo(nextLink)
-			del nextPageLinks[personName]
-	
-	missingJumps = [pageNumber for pageNumber in nextPageLinks if type(pageNumber) is str]
-	if missingJumps:
-		print("Missing required jumps:")
-		print(missingJumps)
-		raise Exception("")
-	
-	# Now write it to file
-	with open(path.join(currentlocation, "POV Cam", "timelines.js"), "w") as outputfile:
-		outputfile.write("peoplenames = ")
-		outputfile.write(str(list(people.keys())))
-		outputfile.write(";\n")
-		
-		outputfile.write("colours = ")
-		outputfile.write(str(colours))
-		outputfile.write(";\n")
-		
-		outputfile.write("images = ")
-		outputfile.write(str(images))
-		outputfile.write(";\n")
-		
-		outputfile.write("groups = ")
-		outputfile.write(str(groups))
-		outputfile.write(";\n")
-		
-		outputfile.write("\ntimelines = {")
-		for pageNumber in sorted(nextPageLinks.keys()):
-			outputfile.write("\n\t" + str(pageNumber) + ": [")
-			for l in nextPageLinks[pageNumber]:
-				outputfile.write(l.outputForJS())
-				outputfile.write(",")
-			outputfile.write("],")
-		outputfile.write("\n}")
-	
-	# Any errors?
-	print("Problems:")
-	
-	existingImages = set(listdir(path.join(currentlocation, "POV Cam", "images")))
-	for image in images:
-		if image in existingImages:
-			existingImages.remove(image)
-		else:
-			print(image + " is missing")
-	for image in existingImages:
-		print(image + " is not expected")
-	
-	for person in peopleWithFiles.symmetric_difference(expectedPeople):
-		if person in peopleWithFiles:
-			print(person + " is not expected")
-		else:
-			print(person + " is missing")
+    # Get order of expected people from designated file
+    # Read timelines in order
+    # Check expected images present
+
+    current_location = path.dirname(__file__)
+
+    people_with_files = set(listdir(path.join(current_location, "Readable Timelines")))
+    expected_people = set()
+
+    # Get information from files
+    with open(path.join(current_location, "timelineexpectedpeople.txt"), "r") as people_file:
+        for line in people_file:
+            if line.strip() != "":
+                name = line.strip() + ".txt"
+                expected_people.add(name)
+                if name in people_with_files:
+                    parse_person_tokens(parse_person_file(path.join(current_location, "Readable Timelines", name)))
+
+    # Pass through, replacing links from relative locations with absolute locations
+    # (Note: still have links to relative locations)
+    for person_name in people:
+        if person_name in next_page_links:
+            for page in next_page_links[person_name]:
+                for next_link in page.next_links:
+                    for last_page in people[person_name].last_pages:
+                        last_page.link_to(next_link)
+            del next_page_links[person_name]
+
+    missing_jumps = [page_number for page_number in next_page_links if isinstance(page_number, str)]
+    if missing_jumps:
+        print("Missing required jumps:")
+        print(missing_jumps)
+        raise Exception("")
+
+    # Now write it to file
+    with open(path.join(current_location, "POV Cam", "timelines.js"), "w") as output_file:
+        output_file.write("peoplenames = ")
+        output_file.write(str(list(people.keys())))
+        output_file.write(";\n")
+
+        output_file.write("colours = ")
+        output_file.write(str(colours))
+        output_file.write(";\n")
+
+        output_file.write("images = ")
+        output_file.write(str(images))
+        output_file.write(";\n")
+
+        output_file.write("groups = ")
+        output_file.write(str(groups))
+        output_file.write(";\n")
+
+        output_file.write("\ntimelines = {")
+        for page_number in sorted(next_page_links.keys()):
+            output_file.write("\n\t" + str(page_number) + ": [")
+            for l in next_page_links[page_number]:
+                output_file.write(l.output_for_js())
+                output_file.write(",")
+            output_file.write("],")
+        output_file.write("\n}")
+
+    # Any errors?
+    print("Problems:")
+
+    existing_images = set(listdir(path.join(current_location, "POV Cam", "images")))
+    for image in images:
+        if image in existing_images:
+            existing_images.remove(image)
+        else:
+            print(image + " is missing")
+    for image in existing_images:
+        print(image + " is not expected")
+
+    for person in people_with_files.symmetric_difference(expected_people):
+        if person in people_with_files:
+            print(person + " is not expected")
+        else:
+            print(person + " is missing")
--- timelineobjects.py
+++ timelineobjects.py
@@ -2,51 +2,61 @@ from collections import defaultdict, OrderedDict
 
 people = OrderedDict()
 
-class person:
-	def __init__(self, _name):
-		self.name = _name
-		if not _name in people:
-			people[_name] = self
-		self.personID = list(people.keys()).index(_name)
-		
-		self.firstPage = None
-		self.lastPages = []
-	
-	def __str__(self):
-		return str(self.name)
 
-def getPerson(personName):
-	if personName in people:
-		return people[personName]
-	else:
-		return person(personName)
+class Person:
+    def __init__(self, name):
+        self.name = name
+        if name not in people:
+            people[name] = self
+        self.person_id = list(people.keys()).index(name)
 
-nextPageLinks = defaultdict(list)
+        self.first_page = None
+        self.last_pages = []
 
-class link:
-	def __init__(self, _currentPageNumber, _person = None, _colourID = None, _imageID = None, _groupID = None):
-		self.currentPageNumber = _currentPageNumber
-		self.person = _person
-		self.colourID = _colourID
-		self.imageID = _imageID
-		self.groupID = _groupID
-		self.nextLinks = []
-		self.nextLinkCaptions = []
-		
-		if type(_person) is person and _person.firstPage == None:
-			_person.firstPage = self
-		
-		nextPageLinks[_currentPageNumber].append(self)
-	
-	def __str__(self):
-		return str(self.currentPageNumber) + " ==> " + \
-			str([nextLink.currentPageNumber if type(nextLink) is link else nextLink for nextLink in self.nextLinks]) + ": " + \
-			str(self.person) + " " + str(self.colourID) + " " + str(self.imageID)
-	
-	def linkTo(self, nextLink, caption = None):
-		self.nextLinks.append(nextLink)
-		self.nextLinkCaptions.append(caption)
-	
-	def outputForJS(self):
-		nextLinks = (nextLink if type(nextLink) is link else getPerson(nextLink).firstPage for nextLink in self.nextLinks)
-		return str([self.person.personID, self.colourID, self.imageID, self.groupID, [[nextLink.currentPageNumber, nextPageLinks[nextLink.currentPageNumber].index(nextLink)] + ([caption] if caption != None else []) for nextLink, caption in zip(nextLinks, self.nextLinkCaptions)]]).replace(" ", "")
+    def __str__(self):
+        return str(self.name)
+
+
+def get_person(person_name):
+    if person_name in people:
+        return people[person_name]
+    else:
+        return Person(person_name)
+
+
+next_page_links = defaultdict(list)
+
+
+class Link:
+    def __init__(self, current_page_number, person=None, colour_id=None, image_id=None, group_id=None):
+        self.current_page_number = current_page_number
+        self.person = person
+        self.colour_id = colour_id
+        self.image_id = image_id
+        self.group_id = group_id
+        self.next_links = []
+        self.next_link_captions = []
+
+        if isinstance(person, Person) and person.first_page is None:
+            person.first_page = self
+
+        next_page_links[current_page_number].append(self)
+
+    def __str__(self):
+        return str(self.current_page_number) + " ==> " + \
+               str([next_link.current_page_number if isinstance(next_link, Link) else next_link for next_link in self.next_links]) + ": " + \
+               str(self.person) + " " + str(self.colour_id) + " " + str(self.image_id)
+
+    def link_to(self, next_link, caption=None):
+        self.next_links.append(next_link)
+        self.next_link_captions.append(caption)
+
+    def output_for_js(self):
+        next_links = (next_link if isinstance(next_link, Link) else get_person(next_link).first_page for next_link in self.next_links)
+        return str([
+            self.person.person_id, self.colour_id, self.image_id, self.group_id,
+            [
+                [next_link.current_page_number, next_page_links[next_link.current_page_number].index(next_link)] + ([caption] if caption is not None else [])
+                for next_link, caption in zip(next_links, self.next_link_captions)
+            ]
+        ]).replace(" ", "")
